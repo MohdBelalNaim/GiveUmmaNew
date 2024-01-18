b1ae3fe2db280fd7aa6357a92cc8921fffd3f83c
Mohd Belal Naim <contactbelalnaim@gmail.com>
2024-01-18 14:46:58 +0530

    Added email and login loader and response

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,35 +1,38 @@
-import React,{useState} from 'react'
-import styles from '../assets/css/navbar.module.css'
-import { FaTimes } from 'react-icons/fa'
+import React, { useState } from "react";
+import styles from "../assets/css/navbar.module.css";
+import { FaTimes } from "react-icons/fa";
 
-import Signup from './AuthModal/Signup';
-import Signin from './AuthModal/Signin';
+import Signup from "./AuthModal/Signup";
+import Signin from "./AuthModal/Signin";
 
-const AuthModal = ({controller}) => {
-    
-    const [authPopup,setAuthPopup] = controller
-    const[signup,setSignup] = useState(true)
-return (
-<div className={`${styles.searchOverlay} flex justify-center items-start`}>
-    <div
-        className="bg-white w-[36%] max-sm:w-[96%] rounded-xl p-4 shadow-xl relative top-5 animate__animated animate__bounceIn">
-        <div className="cursor-pointer" onClick={()=>setAuthPopup(false)}>
-            <FaTimes />
+const AuthModal = ({ controller }) => {
+  const [authPopup, setAuthPopup] = controller;
+  const [signup, setSignup] = useState(true);
+  
+  return (
+    <div className={`${styles.searchOverlay} flex justify-center items-start`}>
+      <div className="bg-white w-[36%] max-sm:w-[96%] rounded-xl p-4 shadow-xl relative top-5 animate__animated animate__bounceIn">
+        <div className="cursor-pointer" onClick={() => setAuthPopup(false)}>
+          <FaTimes />
         </div>
         <div className="grid grid-cols-2">
-            <div className="font-bold border-b-2 text-center py-3 cursor-pointer" onClick={()=> setSignup(false)}
-                >
-                Login
-            </div>
-            <div className="font-bold border-b-2 text-center py-3 cursor-pointer" onClick={()=> setSignup(true)}
-                >
-                Signup
-            </div>
+          <div
+            className="font-bold border-b-2 text-center py-3 cursor-pointer"
+            onClick={() => setSignup(false)}
+          >
+            Login
+          </div>
+          <div
+            className="font-bold border-b-2 text-center py-3 cursor-pointer"
+            onClick={() => setSignup(true)}
+          >
+            Signup
+          </div>
         </div>
-        {signup ? <Signup/>: <Signin/> }
+        {signup ? <Signup controller={controller} /> : <Signin controller={controller} />}
+      </div>
     </div>
-</div>
-)
-}
+  );
+};
 
-export default AuthModal
+export default AuthModal;
--- src/components/AuthModal/Signin.jsx
+++ src/components/AuthModal/Signin.jsx
@@ -1,59 +1,91 @@
-import React,{useState} from 'react'
-import { signInWithPopup,createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { signInWithPopup, signInWithEmailAndPassword } from "firebase/auth";
 import { auth, provider } from "../../utils/firebaseConfig";
-import {toast} from "react-hot-toast"
+import { toast } from "react-hot-toast";
+import { SpinnerCircular } from "spinners-react";
 
-const Signin = () => {
-    const googleSignIn = () =>
+const Signin = ({ controller }) => {
+  const [authPopup, setAuthpopup] = controller;
+  const [loading, setLoading] = useState(false);
+
+  const googleSignIn = () =>
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
-        hideAuth()
-        setMenu(false)
+        toast.success("Signed in successfully!");
+        setAuthpopup(false);
       })
       .catch((error) => {
         console.log(error);
-        hideAuth()
-    });
+        hideAuth();
+      });
 
-    const[emailCred,setEmailCred] = useState("")
-    const[passwordCred,setPasswordCred] = useState("")
+  const [emailCred, setEmailCred] = useState("");
+  const [passwordCred, setPasswordCred] = useState("");
 
-    const emailSignin = () =>{
-        signInWithEmailAndPassword(auth,emailCred,passwordCred)
-        .then(userCredentials=>{
-            const user = userCredentials.user;
-            console.log(user)
-            toast.success("Signed in")
-        })
-        .catch(err=>{
-            console.log(err)
-            toast.error("Invalid email or password")
-        })
-    }
-return (
-<section className="p-4">
-    <div className="text-2xl font-bold mb-4">Login</div>
-    <div className="flex items-center justify-center border-[1px] gap-3 py-2 rounded-lg border-black cursor-pointer"
-        onClick={googleSignIn}>
+  const emailSignin = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    signInWithEmailAndPassword(auth, emailCred, passwordCred)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log(user);
+        toast.success("Signed in");
+        setAuthpopup(false);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Invalid email or password");
+        setLoading(false);
+      });
+  };
+  return (
+    <section className="p-4">
+      <div className="text-2xl font-bold mb-4">Login</div>
+      <div
+        className="flex items-center justify-center border-[1px] gap-3 py-2 rounded-lg border-black cursor-pointer"
+        onClick={googleSignIn}
+      >
         <i className="bi bi-google"></i>
         <div>Continue with Google</div>
-    </div>
-    <div className="text-center text-gray-500  py-1">or</div>
-    <input type="text" className="bg-gray-100 border-gray-300 w-full py-3 px-4 mt-3 shadow-inner rounded-lg"
-        placeholder="Email Address" value={emailCred} onChange={e=>setEmailCred(e.target.value)}
-    />
-    <input type="text" className="bg-gray-100 border-gray-300 w-full py-3 px-4 mt-3 shadow-inner rounded-lg"
-        placeholder="Password" value={passwordCred} onChange={e=>setPasswordCred(e.target.value)}
-    />
-    <button onClick={emailSignin} className="bg-black w-full text-white text-lg rounded-lg py-2 mt-3">
-        Login
-    </button>
-    <div className="text-sm text-center mt-3">
+      </div>
+      <div className="text-center text-gray-500  py-1">or</div>
+      <form onSubmit={(e)=>emailSignin(e)}>
+        <input
+          type="email"
+          className="bg-gray-100 border-gray-300 w-full py-3 px-4 mt-3 shadow-inner rounded-lg"
+          placeholder="Email Address"
+          value={emailCred}
+          onChange={(e) => setEmailCred(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          className="bg-gray-100 border-gray-300 w-full py-3 px-4 mt-3 shadow-inner rounded-lg"
+          placeholder="Password"
+          value={passwordCred}
+          onChange={(e) => setPasswordCred(e.target.value)}
+          required
+        />
+        <button
+          className="bg-black w-full text-white text-lg rounded-lg py-2 mt-3"
+          disabled={loading && true}
+        >
+          {loading ? (
+            <div className="flex justify-center">
+              <SpinnerCircular size={30} color="#ffffff" />
+            </div>
+          ) : (
+            "Sign in"
+          )}
+        </button>
+      </form>
+      <div className="text-sm text-center mt-3">
         Don't have an account? Signup
-    </div>
-</section>
-)
-}
+      </div>
+    </section>
+  );
+};
 
-export default Signin
+export default Signin;
