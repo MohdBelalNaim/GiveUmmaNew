bc0ae5e520e6f08218e73e67cbfa83860a1ae1be
Mohd Belal Naim <contactbelalnaim@gmail.com>
2024-01-22 13:08:07 +0530

    changed pkg json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
-import rehypeRaw from "rehype-raw";
 
 dotenv.config();
 // https://vitejs.dev/config/
